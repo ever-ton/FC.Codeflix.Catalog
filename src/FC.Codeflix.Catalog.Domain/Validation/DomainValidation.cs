936e8ad3ada89628d0960f698ec7cca8efe2f7a1
ever-ton <evertonbezerra_93@hotmail.com>
2023-09-26 17:31:15 -0300

    feat: implement domain validation

---
+++ src/FC.Codeflix.Catalog.Domain/Validation/DomainValidation.cs
@@ -0,0 +1,17 @@
+﻿using FC.Codeflix.Catalog.Domain.Exceptions;
+
+namespace FC.Codeflix.Catalog.Domain.Validation;
+public class DomainValidation
+{
+    public static void NotNull(object? target, string filedName)
+    {
+        if(target is null)
+            throw new EntityValidationException($"{filedName} should not be null");
+    }
+
+    public static void NotNullOrEmpty(string? target, string filedName)
+    {
+        if (string.IsNullOrWhiteSpace(target))
+            throw new EntityValidationException($"{filedName} should not be null or empty");
+    }
+}
