620a2c1f548e747965a822b0fbeaecb1e9a155e0
ever-ton <evertonbezerra_93@hotmail.com>
2023-09-26 17:31:39 -0300

    test: implement domain validation

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Validation/DomainValidationTest.cs
@@ -0,0 +1,69 @@
+﻿using Bogus;
+using Xunit;
+using FC.Codeflix.Catalog.Domain.Validation;
+using FluentAssertions;
+using System;
+using FC.Codeflix.Catalog.Domain.Exceptions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Validation;
+public class DomainValidationTest
+{
+    private Faker Faker { get; set; } = new Faker();
+
+
+    [Fact(DisplayName = nameof(NotNullOk))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullOk()
+    {
+        var value = Faker.Commerce.ProductName();
+        Action action =
+            () => DomainValidation.NotNull(value, "value");
+
+        action.Should().NotThrow();
+    }
+
+    [Fact(DisplayName = nameof(NotNullThrowWhenNull))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullThrowWhenNull()
+    {
+        string? value = null;
+        Action action =
+            () => DomainValidation.NotNull(value, "FieldName");
+
+        action.Should()
+            .Throw<EntityValidationException>()
+            .WithMessage("FieldName should not be null");
+    }
+
+
+    //not be null or empty
+    [Theory(DisplayName = nameof(NotNullOrEmptyThrowWhenEmpty))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    [InlineData("")]
+    [InlineData("  ")]
+    [InlineData(null)]
+    public void NotNullOrEmptyThrowWhenEmpty(string? target)
+    {
+
+        Action action =
+            () => DomainValidation.NotNullOrEmpty(target, "FieldName");
+
+        action.Should()
+            .Throw<EntityValidationException>()
+            .WithMessage("FieldName should not be null or empty");
+    }
+
+    [Fact(DisplayName = nameof(NotNullOrEmptyOk))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    public void NotNullOrEmptyOk()
+    {
+        var target = Faker.Commerce.ProductName();
+        Action action =
+            () => DomainValidation.NotNullOrEmpty(target, "FieldName");
+
+        action.Should().NotThrow();
+    }
+    //min length
+    //max length
+
+}
