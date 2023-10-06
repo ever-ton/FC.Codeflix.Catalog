2906803925d8c1cb64b57d3f1c9e17e2e8911e20
ever-ton <evertonbezerra_93@hotmail.com>
2023-10-06 16:41:34 -0300

    test: create category application test

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -0,0 +1,56 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Threading;
+using Xunit;
+using UseCases = FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.CreateCategory;
+public class CreateCategoryTest
+{
+    [Fact(DisplayName = nameof(CreateCategory))]
+    [Trait("Application", "CreateCategory - Use Cases")]
+    public async void CreateCategory()
+    {
+        //Arrange 
+        var repositoryMock = new Mock<ICategoryRepository>();
+        var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+        var useCase = new UseCases.CreateCategory(
+            repositoryMock.Object,
+            unitOfWorkMock.Object);
+
+        var input = new UseCases.CreateCategoryInput(
+            "Category Name",
+            "Category Description",
+            true
+            );
+
+        //Act 
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        repositoryMock.Verify(
+            repository => repository.Insert(
+                    It.IsAny<Category>(),
+                    It.IsAny<CancellationToken>()
+                ),
+                Times.Once
+            );
+
+        unitOfWorkMock.Verify(
+            unitOfWorkMock => unitOfWorkMock.Commit(It.IsAny<CancellationToken>()),
+            Times.Once
+        );
+
+        //Assert
+        output.Should().NotBeNull();
+        output.Name.Should().Be("Category Name");
+        output.Description.Should().Be("Category Description");
+        output.IsActive.Should().Be(true);
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+    }
+}
