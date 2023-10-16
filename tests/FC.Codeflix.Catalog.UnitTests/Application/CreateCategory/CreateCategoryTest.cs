2491a22fb3901aef36d35ac7013705d0882d8c17
ever-ton <evertonbezerra_93@hotmail.com>
2023-10-16 18:42:31 -0300

    test: refactor CreateCategoryTest

--- tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -9,25 +9,29 @@ using Xunit;
 using UseCases = FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
 
 namespace FC.Codeflix.Catalog.UnitTests.Application.CreateCategory;
+
+[Collection(nameof(CreateCategoryTestFixture))]
 public class CreateCategoryTest
 {
+    private readonly CreateCategoryTestFixture _fixture;
+
+    public CreateCategoryTest(CreateCategoryTestFixture fixture) 
+        => _fixture = fixture;
+
+
     [Fact(DisplayName = nameof(CreateCategory))]
     [Trait("Application", "CreateCategory - Use Cases")]
     public async void CreateCategory()
     {
-        //Arrange 
-        var repositoryMock = new Mock<ICategoryRepository>();
-        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        //Arrange
+        var repositoryMock = _fixture.GetCategoryRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
 
         var useCase = new UseCases.CreateCategory(
             repositoryMock.Object,
             unitOfWorkMock.Object);
 
-        var input = new UseCases.CreateCategoryInput(
-            "Category Name",
-            "Category Description",
-            true
-            );
+        var input = _fixture.GetInput();
 
         //Act 
         var output = await useCase.Handle(input, CancellationToken.None);
@@ -47,9 +51,9 @@ public class CreateCategoryTest
 
         //Assert
         output.Should().NotBeNull();
-        output.Name.Should().Be("Category Name");
-        output.Description.Should().Be("Category Description");
-        output.IsActive.Should().Be(true);
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(input.IsActive);
         output.Id.Should().NotBeEmpty();
         output.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
     }
