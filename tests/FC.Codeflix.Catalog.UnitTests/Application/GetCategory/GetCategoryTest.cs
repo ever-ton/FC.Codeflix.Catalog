623a9b38caf8af2bec7e0106ac8904eb35a205df
ever-ton <evertonbezerra_93@hotmail.com>
2023-11-06 21:36:36 -0300

    test: implement application exceptions

--- tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTest.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 using UseCase = FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+using FC.Codeflix.Catalog.Application.Exceptions;
 
 namespace FC.Codeflix.Catalog.UnitTests.Application.GetCategory;
 
@@ -40,7 +41,7 @@ public class GetCategoryTest
         repositoryMock.Verify(r => r.GetAsync(
            It.IsAny<Guid>(),
            It.IsAny<CancellationToken>()
-           ),Times.Once);
+           ), Times.Once);
 
         output.Should().NotBeNull();
         output.Name.Should().Be(exampleCategory.Name);
@@ -48,5 +49,35 @@ public class GetCategoryTest
         output.IsActive.Should().Be(exampleCategory.IsActive);
         output.Id.Should().Be(exampleCategory.Id);
         output.CreatedAt.Should().Be(exampleCategory.CreatedAt);
+    }
+
+    [Fact(DisplayName = nameof(NotFoundExceptionWhenCategoryDoesntExist))]
+    [Trait("Application", "GetCategory - Use Cases")]
+    public async Task NotFoundExceptionWhenCategoryDoesntExist()
+    {
+        //Arrange
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var fakeCategoryId = Guid.NewGuid();
+
+        repositoryMock
+            .Setup(x => x.GetAsync(
+                It.IsAny<Guid>(),
+                It.IsAny<CancellationToken>()))
+            .ThrowsAsync(
+            new NotFoundException($"Category '{fakeCategoryId}' not found"));
+
+        var input = new UseCase.GetCategoryInput(fakeCategoryId);
+        var useCase = new UseCase.GetCategory(repositoryMock.Object);
+
+        //Act
+        var task = async () 
+            => await useCase.Handle(input, CancellationToken.None);
+
+        //Assert
+        await task.Should().ThrowAsync<NotFoundException>();
+        repositoryMock.Verify(r => r.GetAsync(
+           It.IsAny<Guid>(),
+           It.IsAny<CancellationToken>()
+           ), Times.Once);
     }
 }
